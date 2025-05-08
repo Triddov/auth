d5e19911d9cfe54e6c126474f9a5622cce018c8b
Triddov <akinin.stepan7@gmail.com>
2025-05-08 21:19:19 +0300

    fix(authController): end registration

--- authController.js
+++ authController.js
@@ -1,3 +1,5 @@
+import bcrypt from 'bcrypt'
+import { validationResult } from "express-validator"
 import User from "./models/User.js"
 import Role from "./models/Role.js"
 
@@ -5,9 +7,27 @@ class authController{
 
     async registration(req, res){
         try {
+            const errors = validationResult(req)
+            if (!errors.isEmpty()) {
+                return res.status(400).json({message: 'failed', errors: errors})
+            }
+
+            const { username, password } = req.body
+            const candidate = await User.findOne({username})
+            if (candidate) {
+                return res.status(403).json({message: `User already exists`})
+            }
+
+            const hashedPassword = bcrypt.hashSync(password, 7)
+            const userRole = await Role.findOne({value: "USER"})
+            const user = new User({username, password: hashedPassword, roles: [userRole.value]})
+            await user.save()
+
+            return res.status(201).json({message: `Successfully registered`})
 
         } catch (err) {
-            console.log(err)
+            console.error(err)
+            res.status(400).json("Registration failed")
         }
     }
 
@@ -15,7 +35,8 @@ class authController{
         try {
 
         } catch (err) {
-            console.log(err)
+            console.error(err)
+            res.status(400).json("Login failed")
         }
     }
 
@@ -26,7 +47,8 @@ class authController{
             res.json("it's works")
 
         } catch (err) {
-            console.log(err)
+            console.error(err)
+            res.status(400).json("Registration failed")
         }
     }
 
--- authRouter.js
+++ authRouter.js
@@ -1,9 +1,13 @@
 import { Router } from "express"
+import { check } from "express-validator"
 import controller from "./authController.js"
 
 const router = new Router()
 
-router.post('/reg', controller.registration)
+router.post('/reg', [
+    check("username", "Username is empty").not().isEmpty(),
+    check("password", "Password must be longer than 5 symbols").isLength({min: 6})
+], controller.registration)
 router.post('/auth', controller.login)
 router.get('/users', controller.getUsers)
 
